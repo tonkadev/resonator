c584a0f683f88a9ec8289de0101f075b2793a7ab
RudyL <tonkadev@gmail.com>
2019-09-01 09:28:36 -0700

    Updates

---
+++ precache-manifest.80fe8dbf29e67c546c9c9c35efa3bb54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0718296131badd38dd369f309c00860",
+    "url": "/index.html"
+  },
+  {
+    "revision": "563ccde5047e683a9a98",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6a9c669cfa5fd212be9e",
+    "url": "/static/js/2.3d985d85.chunk.js"
+  },
+  {
+    "revision": "563ccde5047e683a9a98",
+    "url": "/static/js/main.2172f6ae.chunk.js"
+  },
+  {
+    "revision": "98e23bf63a71588332a8",
+    "url": "/static/js/runtime~main.3aefc1a6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
