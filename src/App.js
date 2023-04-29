d193770e79fd6456c6ffe3eda8967dbef52b17fa
Developer <developer@Rudys-Mac-mini.local>
2023-04-29 09:59:57 -0700

    small text change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - Resonator Productions
+          Resonator Productions powered by workers
         </a>
       </header>
     </div>
